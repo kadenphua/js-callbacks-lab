2f2058ce2917541a0a64b8fe4403a266a4c4c2b3
Kaden Liang Zhi Phua <kadenphua@gmail.com>
2016-09-29 09:30:21 +0800

    fixed reset and pause button

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,30 @@
-// your code here:
+window.onload = function () {
+
+  var body = document.querySelector('body');
+  var appendCount = document.getElementById('count')
+  var buttonStart = document.getElementById('start');
+  var buttonStop = document.getElementById('stop');
+  var buttonReset = document.getElementById('reset');
+  var count= 0;
+
+  buttonStart.onclick = function() {
+    x = window.setInterval(startTimer, 1000);
+  }
+
+  buttonStop.onclick = function () {
+    window.clearInterval(x)
+  }
+
+  buttonReset.onclick = function () {
+    window.clearInterval(x);
+    count = 0;
+    h1timer.textContent = "Stop Watch";
+  }
+
+  function startTimer (){
+    var h1timer = document.getElementById('timer');
+    h1timer.textContent =  "Time elapsed " + count;
+    count++;
+  }
+
+}
